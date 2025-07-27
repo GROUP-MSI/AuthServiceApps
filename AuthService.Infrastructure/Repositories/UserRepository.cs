6e4bafa8c7503e392a2aca60a383716aaa158b39
LuisCalleLazo <callelazodeynarluis@gmail.com>
2025-07-26 22:15:57 -0400

    feat(user): Add Repository

---
+++ AuthService.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,56 @@
+using AuthService.Application.Helpers;
+using AuthService.Domain.Entities;
+using AuthService.Domain.Repositories;
+using AuthService.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Infrastructure.Repositories
+{
+  public class UserRepository : IUserRepository
+  {
+    private readonly DataContext _context;
+    public UserRepository(DataContext context)
+    {
+      _context = context;
+    }
+
+    public async Task<UserEntity> GetUserByAuth(string nameOrGmail, string password)
+    {
+      var user = await _context.Users
+        .Where(u => u.Name == nameOrGmail || u.Email == nameOrGmail)
+        .FirstOrDefaultAsync();
+
+      if (user == null) return null;
+
+      if (!PasswordHashSecurity.VerifyPassword(user.Password, password, user.PasswordSalt)) return null;
+
+      return user;
+    }
+
+
+    public async Task<UserEntity> GetUserAuthById(int id) =>
+      await _context.Users
+        .Where(u => u.Id == id)
+        .FirstOrDefaultAsync();
+
+
+    public async Task<UserEntity> CreateUser(UserEntity create, Guid salt)
+    {
+      create.PasswordSalt = salt;
+
+      await _context.Users.AddAsync(create);
+      await _context.SaveChangesAsync();
+      return create;
+    }
+
+    public async Task<UserEntity> GetUserByName(string name) =>
+      await _context.Users
+        .Where(u => u.Name == name)
+        .FirstOrDefaultAsync();
+
+    public async Task<UserEntity> GetUserByEmail(string email) =>
+      await _context.Users
+        .Where(u => u.Email == email)
+        .FirstOrDefaultAsync();
+  }
+}
